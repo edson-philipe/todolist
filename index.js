b6040b883a0b455a66b6373845e44b7cdabf75c8
edson-philipe <146252662+edson-philipe@users.noreply.github.com>
2023-09-28 18:43:53 +0100

    a
    a

--- index.js
+++ index.js
@@ -1,65 +1,37 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
 const Sequelize = require("sequelize");
-const connection = require("./database/database");
-const Tarefa = require("./database/Tarefa");
-
-connection.authenticate().then(() => {
-    console.log('Conectado ao banco de dados!!');
-}).catch((error) => {
-    console.error(error);
-});
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const connection = new Sequelize(
+    'bd6vxflkis8r477dvxzt',
+    'ugvggxef85iupzhw',
+    'gnTp0oU8HWidecLlVYGB',
+    {
+        host: 'bd6vxflkis8r477dvxzt-mysql.services.clever-cloud.com',
+        dialect: 'mysql'
+    }
+);
 
-app.get("/", (req, res) => {
-    Tarefa.findAll(
-        {
-            raw: true, 
-            order: [
-                ['id', 'DESC']
-            ]
-        },
-    ).then(tarefas => {
-        res.render("index", {
-            tarefas: tarefas,
-        });
-    });
-});
-
-app.get("/inserirtarefa", (req, res) => {
-    res.render("inserirTarefa");
-});
-
-app.post("/salvarInsercaoTarefa", (req, res) => {
-    const titulo = req.body.titulo;
-    const descricao = req.body.descricao;
-    Tarefa.create({
-        titulo: titulo,
-        descricao: descricao,
-        concluido: false,
-    }).then(() => {
-        res.redirect('/');
-    });
-});
-
-app.post("/excluirTarefa", (req, res) => {
-    const id = req.body.id;
-    Tarefa.destroy({
-        where: {
-            id: id,
-        }
-    }).then(() => {
-        res.redirect('/');
-    });
+connection.authenticate().then(() => {
+    console.log('Conectado ao banco de dados!');
+}).catch((error) => {
+    console.error('Erro ao conectar ao banco de dados:', error);
 });
 
 
-app.listen(3000, () => {
-    console.log("Servidor rodando!");
+app.get("/", async (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}.`);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // Encerre o aplicativo ou tome outras medidas apropriadas
 });
--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-let inputTarefaConcluida = document.querySelectorAll(".inputTarefaConcluida");
-for (let i = 0; i <= inputTarefaConcluida.length; i++) {
-    if (inputTarefaConcluida[i].value == 1) {
-        inputTarefaConcluida[i].checked = true;
-    } else {
-        inputTarefaConcluida[i].checked = false;
-    }
-
-}
