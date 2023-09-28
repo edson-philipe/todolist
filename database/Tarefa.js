d05f52c65e8717a259dba9982ed502c8917758af
edson-philipe <146252662+edson-philipe@users.noreply.github.com>
2023-09-28 19:08:37 +0100

    a
    a

--- database/Tarefa.js
+++ database/Tarefa.js
@@ -13,6 +13,9 @@ const Tarefa = connection.define("tarefas", {
     concluido: {
         type: DataTypes.BOOLEAN,
     },
+}, {
+    timestamps: false, // Isso desativará a criação automática de createdAt e updatedAt
 });
+
 
 module.exports = Tarefa;
--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,8 @@
 let inputTarefaConcluida = document.querySelectorAll(".inputTarefaConcluida");
-for (let i = 0; i <= inputTarefaConcluida.length; i++) {
+for (let i = 0; i < inputTarefaConcluida.length; i++) {
     if (inputTarefaConcluida[i].value == 1) {
         inputTarefaConcluida[i].checked = true;
     } else {
         inputTarefaConcluida[i].checked = false;
     }
-
 }
