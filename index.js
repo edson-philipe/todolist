97e02069fa009b79466a58c21bdc3c64ed82acd3
edson-philipe <146252662+edson-philipe@users.noreply.github.com>
2023-09-28 17:35:30 +0100

    a
    a

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require("express");
+const Sequelize = require("sequelize");
+
+const connection = new Sequelize(
+    'bd6vxflkis8r477dvxzt',
+    'ugvggxef85iupzhw',
+    'gnTp0oU8HWidecLlVYGB',
+    {
+        host: 'bd6vxflkis8r477dvxzt-mysql.services.clever-cloud.com',
+        dialect: 'mysql'
+    }
+);
+
+connection.authenticate().then(() => {
+    console.log('Conectado ao banco de dados!');
+}).catch((error) => {
+    console.error(error);
+});
 
 // Criando a aplicação Express.
 const app = express();
